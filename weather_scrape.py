6c498efb0bb99d36077dca4ddacedcc8355546c7
Jack Willis <jack.willis@worldcovr.com>
2019-03-19 11:17:39 -0400

    Updated scraper with time steps
    this is the full time step based script

--- weather_scrape.py
+++ weather_scrape.py
@@ -6,14 +6,15 @@ import numpy
 
 if not  os.path.exists("html_files"):
 	os.mkdir("html_files")
+for i in range(120)
+	current_time_step=datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
+	response = urllib.request.urlopen("https://weather.com/weather/hourbyhour/l/29634:4:US")
+	html = response.read()
+	print(html)
+	f=open("html_files/weather" + current_time_step +".html","wb")
+	f.write(html)
 
-current_time_step=datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
-	
-response = urllib.request.urlopen("https://weather.com/weather/hourbyhour/l/29634:4:US")
-	
-html = response.read()
-f=open("html_files/weather" + current_time_step +".html","wb")
-f.write(html)
-f.close()
-print("requesting ")
+	f.close()
+	print("requesting weather channel again this hour")
+	time.sleep(3600)
 
